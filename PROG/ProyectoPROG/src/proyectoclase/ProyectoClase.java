51c005aca709a606efa71ee808eaaa739f2e18d4
PabloKrespo <56074061+pablokrespo@users.noreply.github.com>
2020-06-23 10:54:11 +0200

    Add files via upload

---
+++ PROG/ProyectoPROG/src/proyectoclase/ProyectoClase.java
@@ -0,0 +1,14 @@
+package proyectoclase;
+
+/**
+ *
+ * @author DANIELCASTELAO\mmartinezcosta
+ */
+public class ProyectoClase {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
